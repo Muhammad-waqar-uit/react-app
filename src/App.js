9d99a4b3a065f1aaf6c162d4a4ab6301995bd27a
Muhammad Waqar <mw6069605@gmail.com>
2022-09-05 12:21:55 +0500

    componets and props and jsx understood

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App"> Hello World</div>
-  );
-}
+   return (<div className='app'>
+    <Job salary={30000} Job="React Developer" company="Cubix"/>
+   </div>);
+  }
+
+  const Job =(props)=>{
+    return (
+      <div><h1>{props.salary}</h1>
+      <h1>{props.Job}</h1>
+      <h1>{props.company}</h1>
+      </div>
+    );
+  }
 
 export default App;
